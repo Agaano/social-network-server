c60f6a3e9ab3d2bffaf84eee9a3db2aa7c24996f
Agaano <klapoplapo@gmail.com>
2023-10-06 00:36:14 +0300

    main ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
-
-  app.enableCors({ credentials: true, origin: true });
-  const config = new DocumentBuilder()
-    .setTitle('JJ')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   await app.listen(8000);
 }
