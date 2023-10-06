d1d35341611c1c4fe081a059d43dd9248e356aa6
Agaano <klapoplapo@gmail.com>
2023-10-06 05:30:25 +0300

    dasdas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,8 +17,8 @@ export class AppController {
       return;
     }
     const file = await readFileSync(__dirname + '/uploads/avatars/' + fileName);
-    console.log(__dirname + '/uploads/avatars/' + fileName)
-    console.log(uploadDirectory() + fileName);
+    console.log('Composite ' + __dirname + '/uploads/avatars/' + fileName)
+    console.log('Not composite ' + uploadDirectory() + fileName);
     console.log(__dirname + '/uploads/avatars/' + fileName === uploadDirectory() + fileName);
     response.appendHeader('Content-Type', contType)
     response.write(file);
