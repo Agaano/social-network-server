754749a61c34aa48d39a4bc90a80bc9b0b8c99f1
Agaano <klapoplapo@gmail.com>
2023-10-06 04:49:38 +0300

    uploadDir

--- src/uploads/avatars/uploadDirectory.ts
+++ src/uploads/avatars/uploadDirectory.ts
@@ -1,6 +1,13 @@
 export default () => {
 	console.log(__dirname);
 	const array = __dirname.split(`\\`).reverse();
+	if (array.length === 1) {
+		const anotherarray = __dirname.split('/').reverse();
+		anotherarray[2] = 'src';
+		anotherarray.reverse();
+		const newanotherarray = anotherarray.join('/');
+		return newanotherarray + '/';
+	}
 	array[2] = 'src';
 	array.reverse();
 	const newarray = array.join(`\\`);
