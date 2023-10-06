52e97a18bd17262c4c93a361b6724a29da6a53a6
Agaano <klapoplapo@gmail.com>
2023-10-06 05:10:01 +0300

    asdas

--- src/uploads/avatars/uploadDirectory.ts
+++ src/uploads/avatars/uploadDirectory.ts
@@ -1,15 +1,16 @@
 export default () => {
 	console.log(__dirname);
-	const array = __dirname.split(`\\`).reverse();
-	if (array.length === 1) {
-		const anotherarray = __dirname.split('/').reverse();
-		anotherarray[2] = 'dist';
-		anotherarray.reverse();
-		const newanotherarray = anotherarray.join('/');
-		return newanotherarray + '/';
-	}
-	array[2] = 'src';
-	array.reverse();
-	const newarray = array.join(`\\`);
-	return newarray + '\\';
+	return __dirname + '/';
+	// const array = __dirname.split(`\\`).reverse();
+	// if (array.length === 1) {
+	// 	const anotherarray = __dirname.split('/').reverse();
+	// 	anotherarray[2] = 'dist';
+	// 	anotherarray.reverse();
+	// 	const newanotherarray = anotherarray.join('/');
+	// 	return newanotherarray + '/';
+	// }
+	// array[2] = 'src';
+	// array.reverse();
+	// const newarray = array.join(`\\`);
+	// return newarray + '\\';
 }
